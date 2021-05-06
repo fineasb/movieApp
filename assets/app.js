0e00039d719fd67ea2f9d8e29275d473940547c6
Fineas Berindei <fberindei@dexwell.com>
2021-05-06 05:36:11 -0700

    miau

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,5 @@
 const base = 'https://api.themoviedb.org/3/search/movie?api_key=3e60976c4307fa5a4b76524c4a26318e&query=';
+const IMAGE_URL = "https://image.tmdb.org/t/p/original";
 
 const result = document.querySelector('.search-box');
 
@@ -11,16 +12,42 @@ function setQuery(evt) {
  }
 }
 
+const movieSearchable = document.querySelector('#collections');
+
+function createMovieContainer(value) {
+   const movieElement = document.createElement('div');
+   movieElement.setAttribute('class', 'movie');  
+   const template =  `<div id="image">
+                        ${miau(value)}
+                      </div>`;
+   movieElement.innerHTML = template;
+   return movieElement;
+}
+
 function search(value) {
     fetch(`https://api.themoviedb.org/3/search/movie?api_key=3e60976c4307fa5a4b76524c4a26318e&query=${value}`)
-  .then(response => response.json())
-  .then(result =>
-        console.log(result.results))
-  .catch(error => console.log('error', error));
+    .then(response => response.json())
+    .then(result =>{
+            const movies = result.results;
+            const movieBlk = createMovieContainer(movies)
+            movieSearchable.appendChild(movieBlk);
+            console.log(movieBlk);
+        })
+    .catch(error => console.log('error', error));
 }
+
+ function miau(elements){
+      return elements.map(el => {
+        return `<img src='${IMAGE_URL}${el.poster_path}' alt="Girl in a jacket" width="500" height="600">`});
+ }
+
+
+
+
+
+
+
 
 function searchable() {
     search(result.value);
 }
-
-
