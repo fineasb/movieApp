2312892dc66539bebf6732572523903a7f0e0a90
Fineas Berindei <fberindei@dexwell.com>
2021-05-07 04:38:37 -0700

    all done

--- assets/app.js
+++ assets/app.js
@@ -1,16 +1,49 @@
+const base = 'https://api.themoviedb.org/3/search/movie?api_key=3e60976c4307fa5a4b76524c4a26318e&query=';
+const IMAGE_URL = "https://image.tmdb.org/t/p/original";
+
 const result = document.querySelector('#search-box');
 const navbar = document.querySelector('#navbarFineas');
+const colections = document.querySelector('#collection');
 
 result.addEventListener('keypress', setQuery);
 
 
 function setQuery(evt) {
     if(evt.keyCode == 13){
-       console.log(result.value);
+       search(result.value);
        evt.preventDefault();
        result.value = '';
        navbar.className = "navbar-collapse collapse";
     }
+}
+
+function searchable(){
+    search(result.value);
+    result.value = '';
+    navbar.className = "navbar-collapse collapse";
+}
+
+function search(value) {
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=3e60976c4307fa5a4b76524c4a26318e&query=${value}`)
+    .then(response => response.json())
+    .then(rendersis)
+    .catch(error => console.log('error', error));
+}
+
+function rendersis(value){
+    const view = render(value);
+    view.map(el => {
+        colections.innerHTML = el;
+
+    })
+}
+
+function render(value){
+    const movies = value.results;
+    return movies.map(el=> {
+        return `<div id="lol"><p id="title">You searched for:</p>
+        <img src='${IMAGE_URL + el.poster_path}' alt="Girl in a jacket" width="500" height="600"></div>`
+    });
 }
 
 
@@ -32,32 +65,6 @@ function setQuery(evt) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const base = 'https://api.themoviedb.org/3/search/movie?api_key=3e60976c4307fa5a4b76524c4a26318e&query=';
-// const IMAGE_URL = "https://image.tmdb.org/t/p/original";
-
-// const result = document.querySelector('.search-box');
-
-// result.addEventListener('keypress', setQuery);
-
-// function setQuery(evt) {
-//      if(evt.keyCode == 13){
-//         search(result.value);
-//         result.value= '';
-//  }
-// }
 
 // const movieSearchable = document.querySelector('#collections');
 
@@ -91,7 +98,3 @@ function setQuery(evt) {
 //         return `<img src='${IMAGE_URL + el.poster_path}' alt="Girl in a jacket" width="500" height="600">`}});
 //       }
 
-// function searchable() {
-//     search(result.value);
-//     result.value= '';
-// }
