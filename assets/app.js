66125ae2fbf9f60b0f8e21070d82f990d8e34272
Fineas Berindei <fberindei@dexwell.com>
2021-05-06 22:22:44 -0700

    navbar

--- assets/app.js
+++ assets/app.js
@@ -3,20 +3,27 @@ const IMAGE_URL = "https://image.tmdb.org/t/p/original";
 
 const result = document.querySelector('.search-box');
 
-
 result.addEventListener('keypress', setQuery);
 
 function setQuery(evt) {
      if(evt.keyCode == 13){
         search(result.value);
+        result.value= '';
  }
 }
 
 const movieSearchable = document.querySelector('#collections');
 
+function renderSearcMovies(result) {
+            movieSearchable.innerHTML = ' ';
+            const movies = result.results;
+            const movieBlk = createMovieContainer(movies)
+            movieSearchable.appendChild(movieBlk);
+}
+
 function createMovieContainer(value) {
    const movieElement = document.createElement('div');
-   movieElement.setAttribute('class', 'movie');  
+   movieElement.setAttribute('class', 'movie');
    const template =  `<div id="image">
                         ${miau(value)}
                       </div>`;
@@ -27,27 +34,17 @@ function createMovieContainer(value) {
 function search(value) {
     fetch(`https://api.themoviedb.org/3/search/movie?api_key=3e60976c4307fa5a4b76524c4a26318e&query=${value}`)
     .then(response => response.json())
-    .then(result =>{
-            const movies = result.results;
-            const movieBlk = createMovieContainer(movies)
-            movieSearchable.appendChild(movieBlk);
-            console.log(movieBlk);
-        })
+    .then(renderSearcMovies)
     .catch(error => console.log('error', error));
 }
 
  function miau(elements){
       return elements.map(el => {
-        return `<img src='${IMAGE_URL}${el.poster_path}' alt="Girl in a jacket" width="500" height="600">`});
- }
-
-
-
-
-
-
-
+            if(el.poster_path){
+        return `<img src='${IMAGE_URL + el.poster_path}' alt="Girl in a jacket" width="500" height="600">`}});
+      }
 
 function searchable() {
     search(result.value);
+    result.value= '';
 }
